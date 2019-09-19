e0a9857d753589eec858d24307b71184ec9cac37
fan-w-suzuki <w_suzuki@fancs.com>
2019-09-19 10:10:43 +0900

    Added new script for managing "upstream-push"

---
+++ upstream-push.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+  echo "指定された引数は$#個です。" 1>&2
+  echo "引数は2個です。" 1>&2
+  echo "1つ目の引数にミラー元のリポジトリを指定してください" 1>&2
+  echo "2つ目の引数にミラー先のリポジトリを指定してください" 1>&2
+  exit 1
+fi
+
+SOURCE_REPOSITORY_NAME="$1"
+TARGET_REPOSITORY_NAME="$2"
+
+function upstreaming {
+    git clone --mirror git@github.com:fan-ADN/${SOURCE_REPOSITORY_NAME}.git
+    cd ${SOURCE_REPOSITORY_NAME}.git
+    git fetch -p origin
+
+    git for-each-ref --format="ref=%(refname)" --shell refs/pull | \
+    while read entry
+    do
+        eval $entry
+        git update-ref -d $ref
+    done
+
+    git remote add upstream origin git@github.com:fan-ADN/${TARGET_REPOSITORY_NAME}.git
+    git push upstream master
+    cd -
+    rm -rf ${SOURCE_REPOSITORY_NAME}.git
+}
+
+upstreaming ${SOURCE_REPOSITORY_NAME} ${TARGET_REPOSITORY_NAME}
