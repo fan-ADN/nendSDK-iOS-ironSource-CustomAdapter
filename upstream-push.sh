0a11406eb3c0b9602a44170a7d9783a61ebd19f7
fan-w-suzuki <w_suzuki@fancs.com>
2019-09-26 11:43:35 +0900

    Updated script : up-streaming

--- upstream-push.sh
+++ upstream-push.sh
@@ -17,7 +17,10 @@ function upstreaming {
     git config user.name "fan-bot-a8kun"
     git config user.email "bot-a8kun@fancs.com" # <- このメルアドは存在しません(・∀・)b
 
+    git remote set-branches origin 'heads/*'
+    git remote set-url --push origin git@github.com:fan-ADN/${SOURCE_REPOSITORY_NAME}.git
     git fetch -p origin
+
     git for-each-ref --format="ref=%(refname)" --shell refs/pull | \
     while read entry
     do
@@ -25,10 +28,20 @@ function upstreaming {
         git update-ref -d $ref
     done
 
-    git remote add upstream origin git@github.com:fan-ADN/${TARGET_REPOSITORY_NAME}.git
-    git push upstream master
-    cd -
+    git push --mirror
+    cd ..
     rm -rf ${SOURCE_REPOSITORY_NAME}.git
+
+    git clone git@github.com:fan-ADN/${TARGET_REPOSITORY_NAME}.git
+    cd ${TARGET_REPOSITORY_NAME}
+    git config user.name "fan-bot-a8kun"
+    git config user.email "bot-a8kun@fancs.com" # <- このメルアドは存在しません(・∀・)b
+
+    git remote add upstream git@github.com:fan-ADN/${SOURCE_REPOSITORY_NAME}.git
+    git pull upstream master --no-edit
+    git push origin master
+    cd ..
+    rm -rf ${TARGET_REPOSITORY_NAME}
 }
 
 upstreaming ${SOURCE_REPOSITORY_NAME} ${TARGET_REPOSITORY_NAME}
